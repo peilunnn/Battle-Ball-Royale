532ee1e98042137be6b1aac6b9420a1eafe0a291
peilunnn <peilunnn@gmail.com>
2021-08-01 20:45:03 +0800

    fixed vscode autocomplete and scene view

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
@@ -10,7 +10,7 @@ public class PlayerMovement : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

e1290daa0195564952b728bef90a60c761d7f578
peilunnn <peilunnn@gmail.com>
2021-08-01 20:25:34 +0800

    added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
